177e2b6453cdd2fa4b3f03bd68c777e8eb154cfd
vid Strickland <vidkid7@gmail.com>
2017-06-06 23:01:32 +0000

    Adding post-Sanctuary descriptions

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router          = express.Router();
 var mongoose        = require("mongoose");
 var sanctDescription1 = require("../models/sanctuaryDesc1");
 var sanctDescription2 = require("../models/sanctuaryDesc2");
+var sanctDescription3 = require("../models/sanctuaryDesc3");
+var sanctDescription4 = require("../models/sanctuaryDesc4");
+var sanctDescription5 = require("../models/sanctuaryDesc5");
 //var User            = require("../models/user");
 //test
 
@@ -10,9 +13,15 @@ var sanctDescription2 = require("../models/sanctuaryDesc2");
 router.get("/", function(req, res){
     sanctDescription1.find({},{},function(err,sanctuaryDesc1){
         sanctDescription2.find({},{},function(err,sanctuaryDesc2){
-        res.render("landing.ejs", {
-            "sanctuaryDesc1" : sanctuaryDesc1,
-            "sanctuaryDesc2" : sanctuaryDesc2,
+            sanctDescription3.find({},{},function(err,sanctuaryDesc3){
+                sanctDescription4.find({},{},function(err,sanctuaryDesc4){
+                    sanctDescription5.find({},{},function(err,sanctuaryDesc5){
+                    res.render("landing.ejs", {
+                        "sanctuaryDesc1" : sanctuaryDesc1,
+                        "sanctuaryDesc2" : sanctuaryDesc2,
+                        })
+                    })
+                })
             })
         })
     });
@@ -46,10 +55,22 @@ router.post("/admin", function(req, res){
     var newDesc2 = new sanctDescription2({
         sanctuaryDesc2: req.body.sanctuaryDesc2,
     });
+    var newDesc3 = new sanctDescription3({
+        sanctuaryDesc3: req.body.sanctuaryDesc3,
+    });
+    var newDesc4 = new sanctDescription4({
+        sanctuaryDesc4: req.body.sanctuaryDesc4,
+    });
+    var newDesc5 = new sanctDescription5({
+        sanctuaryDesc5: req.body.sanctuaryDesc5,
+    });
     
     //sanctDescription.update({currentDPCost: req.body.dpcost});
     newDesc1.save();
     newDesc2.save();
+    newDesc3.save();
+    newDesc4.save();
+    newDesc5.save();
    //console.log(req.body.dpcost);
    res.redirect("/admin");
 });
