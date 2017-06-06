c778fdde42ba6715378ddc86a5d6da0195997eb3
vid Strickland <vidkid7@gmail.com>
2017-06-06 21:45:22 +0000

    Randomize Both Sanctuary Descriptors

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,21 @@
 var express = require("express");
 var router          = express.Router();
 var mongoose        = require("mongoose");
-var sanctDescription = require("../models/sanctuaryDesc");
+var sanctDescription1 = require("../models/sanctuaryDesc1");
+var sanctDescription2 = require("../models/sanctuaryDesc2");
 //var User            = require("../models/user");
 //test
 
 //Root Route
 router.get("/", function(req, res){
-    sanctDescription.find({},{},function(err,sanctuaryDesc){
+    sanctDescription1.find({},{},function(err,sanctuaryDesc1){
         res.render("landing.ejs", {
-            "sanctuaryDesc1" : sanctuaryDesc,
-            "sanctuaryDesc2" : sanctuaryDesc
+            "sanctuaryDesc1" : sanctuaryDesc1,
+        });
+    })
+    sanctDescription2.find({},{},function(err,sanctuaryDesc2){
+        res.render("landing.ejs", {
+            "sanctuaryDesc2" : sanctuaryDesc2,
         });
     })
 });
@@ -37,13 +42,16 @@ router.get("/admin", function(req, res){
 
 //admin logic
 router.post("/admin", function(req, res){
-    var newDesc1 = new sanctDescription({
+    var newDesc1 = new sanctDescription1({
         sanctuaryDesc1: req.body.sanctuaryDesc1,
+    });
+    var newDesc2 = new sanctDescription2({
         sanctuaryDesc2: req.body.sanctuaryDesc2,
     });
     
     //sanctDescription.update({currentDPCost: req.body.dpcost});
     newDesc1.save();
+    newDesc2.save();
    //console.log(req.body.dpcost);
    res.redirect("/admin");
 });
