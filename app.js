f4043aab934ad76d207914f6fa4662323e7c3b23
vid Strickland <vidkid7@gmail.com>
2017-06-06 01:48:27 +0000

    added listener

--- app.js
+++ app.js
@@ -16,3 +16,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"))
 app.use(methodOverride("_method"));
 app.use(flash());
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Your Sanctuary server has started");
+});
